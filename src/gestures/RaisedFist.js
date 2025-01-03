9ef2a404b0515f66f4974a8687f7577b93adf9f5
Rahul naik <sabhawatrahu82@gmail.com>
2025-01-03 10:28:41 +0530

    changed features

--- src/gestures/RaisedFist.js
+++ src/gestures/RaisedFist.js
@@ -1,10 +1,16 @@
 import * as fp from "fingerpose";
 
-const RaisedFistGesture = new fp.GestureDescription('raised_fist');
+const RaisedFistGesture = new fp.GestureDescription("No");
 
-for (let finger of [fp.Finger.Thumb, fp.Finger.Index, fp.Finger.Middle, fp.Finger.Ring, fp.Finger.Pinky]) {
-    RaisedFistGesture.addCurl(finger, fp.FingerCurl.FullCurl, 1.0);
-    RaisedFistGesture.addCurl(finger, fp.FingerCurl.HalfCurl, 0.9);
+for (let finger of [
+  fp.Finger.Thumb,
+  fp.Finger.Index,
+  fp.Finger.Middle,
+  fp.Finger.Ring,
+  fp.Finger.Pinky,
+]) {
+  RaisedFistGesture.addCurl(finger, fp.FingerCurl.FullCurl, 1.0);
+  RaisedFistGesture.addCurl(finger, fp.FingerCurl.HalfCurl, 0.9);
 }
 
 export default RaisedFistGesture;
--- src/gestures/RaisedHand.js
+++ src/gestures/RaisedHand.js
@@ -1,13 +1,26 @@
 import * as fp from "fingerpose";
 
-const RaisedHandGesture = new fp.GestureDescription('raised_hand');
+const RaisedHandGesture = new fp.GestureDescription("hello");
 
-for (let finger of [fp.Finger.Thumb, fp.Finger.Index, fp.Finger.Middle, fp.Finger.Ring, fp.Finger.Pinky]) {
-    RaisedHandGesture.addCurl(finger, fp.FingerCurl.NoCurl);
-    RaisedHandGesture.addDirection(finger, fp.FingerDirection.VerticalUp, 1.0);
-    RaisedHandGesture.addDirection(finger, fp.FingerDirection.DiagonalUpLeft, 1.0);
-    RaisedHandGesture.addDirection(finger, fp.FingerDirection.DiagonalUpRight, 1.0);
-
+for (let finger of [
+  fp.Finger.Thumb,
+  fp.Finger.Index,
+  fp.Finger.Middle,
+  fp.Finger.Ring,
+  fp.Finger.Pinky,
+]) {
+  RaisedHandGesture.addCurl(finger, fp.FingerCurl.NoCurl);
+  RaisedHandGesture.addDirection(finger, fp.FingerDirection.VerticalUp, 1.0);
+  RaisedHandGesture.addDirection(
+    finger,
+    fp.FingerDirection.DiagonalUpLeft,
+    1.0
+  );
+  RaisedHandGesture.addDirection(
+    finger,
+    fp.FingerDirection.DiagonalUpRight,
+    1.0
+  );
 }
 
 export default RaisedHandGesture;
